af91aeefac1f0d5dc68835cb88be14878fc8305c
Avinaba <avinabad21@gmail.com>
2025-05-11 22:32:04 +0530

    new files class component

--- classcounterreactapp/src/App.js
+++ classcounterreactapp/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Counter from './component/Counter';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Counter/>
     </div>
   );
 }
---
+++ classcounterreactapp/src/component/Counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state
+    this.state = {
+      count: 0,
+    };
+  }
+
+  // Method to increment count
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = ()=>{
+    this.setState(prevState =>({
+        count: prevState.count -1,
+    }));
+  };
+
+  render(){
+    return(
+        <div>
+            <h2>Counter App</h2>
+            <p>Count:{this.state.count}</p>
+            <button onClick={this.increment}>Increment</button>
+            <button onClick={this.decrement}>Increment</button>
+        </div>
+    )
+  }
+};
+export default Counter;
